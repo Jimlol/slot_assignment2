8e55f933135425de9303d2ea967bc32d0d6b923e
jimlol <hanjingjun891015@gmail.com>
2014-10-31 15:12:56 -0400

    writting java script for the project

---
+++ web_assignment2/js/main.js
@@ -0,0 +1,266 @@
+﻿/// <reference path="jquery.js" />
+var playerMoney = 1000;
+var winnings = 0;
+var jackpot = 5000;
+var turn = 0;
+var playerBet = 0;
+var winNumber = 0;
+var lossNumber = 0;
+var spinResult;
+var fruits = "";
+var winRatio = 0;
+var grapes = 0;
+var bananas = 0;
+var oranges = 0;
+var cherries = 0;
+var bars = 0;
+var bells = 0;
+var sevens = 0;
+var blanks = 0;
+ 
+/* Utility function to show Player Stats */
+function showPlayerStats()
+{
+    winRatio = winNumber / turn;
+    $("#jackpot").text("Jackpot: " + jackpot);
+    $("#playerMoney").text(playerMoney);
+    $("#playerTurn").text("Turn: " + turn);
+    $("#playerWins").text("Wins: " + winNumber);
+    $("#playerLosses").text("Losses: " + lossNumber);
+    $("#playerWinRatio").text("Win Ratio: " + (winRatio * 100).toFixed(2) + "%");
+}
+
+/* Utility function to reset all fruit tallies */
+function resetFruitTally() {
+    grapes = 0;
+    bananas = 0;
+    oranges = 0;
+    cherries = 0;
+    bars = 0;
+    bells = 0;
+    sevens = 0;
+    blanks = 0;
+}
+
+/* Utility function to reset the player stats */
+function resetAll() {
+    playerMoney = 1000;
+    winnings = 0;
+    jackpot = 5000;
+    turn = 0;
+    playerBet = 0;
+    winNumber = 0;
+    lossNumber = 0;
+    winRatio = 0;
+}
+
+
+/* Check to see if the player won the jackpot */
+function checkJackPot() {
+    /* compare two random values */
+    var jackPotTry = Math.floor(Math.random() * 51 + 1);
+    var jackPotWin = Math.floor(Math.random() * 51 + 1);
+    if (jackPotTry == jackPotWin) {
+        alert("You Won the $" + jackpot + " Jackpot!!");
+        playerMoney += jackpot;
+        jackpot = 1000;
+    }
+}
+
+/* Utility function to show a win message and increase player money */
+function showWinMessage() {
+    playerMoney += winnings;
+/* change to show picture for win or lose  */
+	$("div.wltips").html('<img src="img/qq_win.png" />');
+/* change txt type to html type to display diffrent color */
+    $("div#winOrLose>p").html("You Won: <span style='color:#f30;'>$" + winnings + "</span>");
+    resetFruitTally();
+    checkJackPot();
+}
+
+/* Utility function to show a loss message and reduce player money */
+function showLossMessage() {
+    playerMoney -= playerBet;
+/* change to show picture for win or lose  */
+	$("div.wltips").html('<img src="img/qq_los.png" />');
+    $("div#winOrLose>p").text("You Lost!");
+    resetFruitTally();
+}
+
+/* Utility function to check if a value falls within a range of bounds */
+function checkRange(value, lowerBounds, upperBounds) {
+    if (value >= lowerBounds && value <= upperBounds)
+    {
+        return value;
+    }
+    else {
+        return !value;
+    }
+}
+
+/* When this function is called it determines the betLine results.
+e.g. Bar - Orange - Banana */
+function Reels() {
+    var betLine = [" ", " ", " "];
+    var outCome = [0, 0, 0];
+
+    for (var spin = 0; spin < 3; spin++) {
+        outCome[spin] = Math.floor((Math.random() * 65) + 1);
+        switch (outCome[spin]) {
+            case checkRange(outCome[spin], 1, 27):  // 41.5% probability
+                betLine[spin] = "0";
+                blanks++;
+                break;
+            case checkRange(outCome[spin], 28, 37): // 15.4% probability
+                betLine[spin] = "1";
+                grapes++;
+                break;
+            case checkRange(outCome[spin], 38, 46): // 13.8% probability
+                betLine[spin] = "2";
+                bananas++;
+                break;
+            case checkRange(outCome[spin], 47, 54): // 12.3% probability
+                betLine[spin] = "3";
+                oranges++;
+                break;
+            case checkRange(outCome[spin], 55, 59): //  7.7% probability
+                betLine[spin] = "4";
+                cherries++;
+                break;
+            case checkRange(outCome[spin], 60, 62): //  4.6% probability
+                betLine[spin] = "5";
+                bars++;
+                break;
+            case checkRange(outCome[spin], 63, 64): //  3.1% probability
+                betLine[spin] = "6";
+                bells++;
+                break;
+            case checkRange(outCome[spin], 65, 65): //  1.5% probability
+                betLine[spin] = "7";
+                sevens++;
+                break;
+        }
+    }
+    return betLine;
+}
+
+/* This function calculates the player's winnings, if any */
+/* change words to number so it can display by picture and no more functions */
+function determineWinnings()
+{
+    if (blanks == 0)
+    {
+        if (grapes == 3) {
+            winnings = playerBet * 10;
+        }
+        else if(bananas == 3) {
+            winnings = playerBet * 20;
+        }
+        else if (oranges == 3) {
+            winnings = playerBet * 30;
+        }
+        else if (cherries == 3) {
+            winnings = playerBet * 40;
+        }
+        else if (bars == 3) {
+            winnings = playerBet * 50;
+        }
+        else if (bells == 3) {
+            winnings = playerBet * 75;
+        }
+        else if (sevens == 3) {
+            winnings = playerBet * 100;
+        }
+        else if (grapes == 2) {
+            winnings = playerBet * 2;
+        }
+        else if (bananas == 2) {
+            winnings = playerBet * 2;
+        }
+        else if (oranges == 2) {
+            winnings = playerBet * 3;
+        }
+        else if (cherries == 2) {
+            winnings = playerBet * 4;
+        }
+        else if (bars == 2) {
+            winnings = playerBet * 5;
+        }
+        else if (bells == 2) {
+            winnings = playerBet * 10;
+        }
+        else if (sevens == 2) {
+            winnings = playerBet * 20;
+        }
+        else if (sevens == 1) {
+            winnings = playerBet * 5;
+        }
+        else {
+            winnings = playerBet * 1;
+        }
+        winNumber++;
+        showWinMessage();
+    }
+    else
+    {
+        lossNumber++;
+        showLossMessage();
+    }
+    
+}
+
+/* When the player clicks the spin button the game kicks off */
+$("#spinButton").click(function () {
+    playerBet = $("div#betEntry>input").val();
+
+    if (playerMoney == 0)
+    {
+        if (confirm("You ran out of Money! \nDo you want to play again?")) {
+            resetAll();
+            showPlayerStats();
+        }
+    }
+    else if (playerBet > playerMoney) {
+        alert("You don't have enough Money to place that bet.");
+    }
+    else if (playerBet < 0) {
+        alert("All bets must be a positive $ amount.");
+    }
+    else if (playerBet <= playerMoney) {
+        spinResult = Reels();
+/* I have change to display one by one and display with some animation */
+        result_fir = spinResult[0];
+		result_sec = spinResult[1];
+		result_thr = spinResult[2];
+
+/* 
+v1.$("div#result_fir>p").text(result_fir);---display text type;
+v2.$("div#result_fir").css("backgroundPositionY", -result_fir * 150);---display with animation;
+v3.$("div#result_fir").animate({backgroundPositionY: -result_fir * 150 - 300} , "fast");---move if the result have no change;
+v3.$("div#result_fir").animate({backgroundPositionY: -result_fir * 150} , "slow");---diplay the right picture after move
+
+*/
+//		$("div#result_fir>p").text(result_fir);
+//		$("div#result_fir").css("backgroundPositionY", -result_fir * 150);
+		$("div#result_fir").animate({backgroundPositionY: -result_fir * 150 - 300} , "fast");
+		$("div#result_fir").animate({backgroundPositionY: -result_fir * 150} , "slow");
+
+//		$("div#result_sec>p").text(result_sec);
+//		$("div#result_sec").css("backgroundPositionY", -result_sec * 150);
+		$("div#result_sec").animate({backgroundPositionY: -result_sec * 150 - 300} , "fast");
+		$("div#result_sec").animate({backgroundPositionY: -result_sec * 150} , "slow");
+
+//		$("div#result_thr>p").text(result_thr);
+//		$("div#result_thr").css("backgroundPositionY", -result_thr * 150);
+		$("div#result_thr").animate({backgroundPositionY: -result_thr * 150 - 300} , "fast");
+		$("div#result_thr").animate({backgroundPositionY: -result_thr * 150} , "slow");
+
+        determineWinnings();
+        turn++;
+        showPlayerStats();
+    }
+    else {
+        alert("Please enter a valid bet amount");
+    }
+    
+});
